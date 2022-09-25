a9bf359afcca6711718bfcbc33922807498715fe
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-09-25 21:21:49 +0545

    add tailwind and images

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=' font-medium text-slate-500'>This is a footer section</div>
+  )
+}
+
+export default Footer
---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>This is a about section</div>
+  )
+}
+
+export default about
